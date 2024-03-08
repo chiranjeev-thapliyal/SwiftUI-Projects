904b8d394622c9a4bbdfaedaca13a942bcf425a9
Chiranjeev Thapliyal <6258860@gmail.com>
2024-03-08 13:33:43 +0530

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Chiranjeev Thapliyal on 08/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
