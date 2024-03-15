c79bf711c7715cb300276b262e66b0adbd9977f0
Chiranjeev Thapliyal <6258860@gmail.com>
2024-03-15 16:30:32 +0530

    initial landmarks setup

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Chiranjeev Thapliyal on 15/03/24.
+//
+
+import SwiftUI
+
+@main // This the entry point for the SwiftUI App
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
